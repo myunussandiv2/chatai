f9a91df7554c80e806823dc017cca5808be9f5f6
myunussandiv2 <96471397+myunussandiv2@users.noreply.github.com>
2023-02-04 15:00:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  OPENAI_API_KEY: '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'
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY
 }
+
+// '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'
 
 // "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"
